49eb0fd847103eb1ace636929b47ee7e421678c4
pielco11 <francesco.poldi@protonmail.ch>
2017-08-24 12:41:14 +0200

    fix

--- setVar.sh
+++ setVar.sh
@@ -4,20 +4,20 @@ echo "[?] Use the compiled version? [Yes/No]"
 read compiled
 if [ $compiled = "Yes" ]
 then
-  echo "compiled = " ${compiled} >> Onionscan/src/Onionscan/transforms/envVar.py
+  echo "compiled = '" + ${compiled} + "'" >> Onionscan/src/Onionscan/transforms/envVar.py
   echo "[?] Input Full pathToCompiled:"
   read pathToCompiled
-  echo "pathToCompiled = " ${pathToCompiled} >> Onionscan/src/Onionscan/transforms/envVar.py
+  echo "pathToCompiled = '" +${pathToCompiled} + "'" >> Onionscan/src/Onionscan/transforms/envVar.py
   echo "[?] Input Full pathToReports:"
   read pathToReports
-  echo "pathToReports = " ${pathToReports} >> Onionscan/src/Onionscan/transforms/envVar.py
+  echo "pathToReports = '" ${pathToReports} + "'" >> Onionscan/src/Onionscan/transforms/envVar.py
   echo "##################################"
   echo "[+] PATH COMPILED setted: " + ${pathToCompiled}
   echo "[+] PATH TO REPORTS setted: " + ${pathToReports}
 else
   echo "[?] Input Full PathToOnionscan: "
   read PathToOnionscan
-  echo "PathToOnionscan = " ${PathToOnionscan} >> Onionscan/src/Onionscan/transforms/envVar.py
+  echo "PathToOnionscan = '" ${PathToOnionscan} + "'" >> Onionscan/src/Onionscan/transforms/envVar.py
   echo "##################################"
   echo "[+] GOPATH setted: " ${PathToOnionscan}
 fi
